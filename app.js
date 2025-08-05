8b0d16ad9721daf0745a0fb6bc87f8ac86eb0b51
Cras-kal <brian0912.vallejo@gmail.com>
2025-08-05 22:03:45 +0800

    addeed my video edit side

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ window.addEventListener('load', () => {
     const hero = document.querySelector('.hero');
     const navbar = document.querySelector('.navbar');
     const heroProject = document.querySelector('.heroProject');
+    const heroEditor = document.querySelector('.heroEditor');
 
 
     if (hero) hero.classList.add('visible');
     if (navbar) navbar.classList.add('visible');
     if (heroProject) heroProject.classList.add('visible');
+    if (heroEditor) heroEditor.classList.add('visible');
 });
